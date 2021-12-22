c7d0c19990424c72f17be1812e04f9569f42c578
WANJIKU NGARUIYA <rngaruiya@codingdojo.com>
2021-12-22 11:00:11 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("page loaded...");
+
+function message() {
+    alert("Ninja was liked!");
+}
+
+function login(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }
+}
+
+function hide(el) {
+    el.remove();
+}
